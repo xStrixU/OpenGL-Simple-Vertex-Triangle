0ddd52b3af5bef9d08ac8b8a14a63767d9811016
xStrixU <strixu155@gmail.com>
2021-04-20 08:52:55 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# OpenGL-Simple-Vertex-Triangle
+Simple triangle rendered by OpenGL using VAO and VBO.
+
+[![](https://i.postimg.cc/W3JSLPrs/Open-GL-Vertex-Triangle.png)](https://i.postimg.cc/W3JSLPrs/Open-GL-Vertex-Triangle.png)
---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+    if(!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW!" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Simple OpenGL Vertex Triangle", nullptr, nullptr);
+
+    if(!window)
+    {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD!" << std::endl;
+        return -1;
+    }
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
